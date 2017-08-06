e69128a3378c50eb593fd4fec2705ab7ae6b1c27
suli <suli@meili-inc.com>
2017-08-06 20:22:27 +0800

    stack

---
+++ Stack/stack.cc
@@ -0,0 +1,22 @@
+/**
+ * Created by suli on 8/6/17.
+ */
+
+#include <iostream>
+
+int function1(int a, int b) {
+	int c = a;
+	return 1;
+}
+
+void function2(int a) {
+	int first = 5;
+	int second = 6;
+	int ret = function1(first, second);
+}
+
+int main(void) {
+	int tmp = 1;
+	int ret = function2(tmp);
+	return 0;
+}
