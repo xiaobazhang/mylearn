c252de0c8185f8a59dac60d28a8d41ef8cea95d0
suli <suli@meili-inc.com>
2017-12-29 19:31:35 +0800

    stack

--- tool/package.sh
+++ tool/package.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
 
+Path="/Users/suli/window/mnt/tool/"
+source="package/app_module_dntg.td3guoPackage"
+name="app_module_dntg.td3guoPackage"
+file="app.module.dntg.controller.DntgController"
+
+screte="AAAAB3NzaC1yc2EAAAABIwAAAQEArk8etQ7S/Uj2QOrzhw54df6LQttJLvRR5qsLLrVxOE5mBG2tl6Q+A4Pj044VinCSRCCwTmUJZRHBOpfN3S1Iuj8teYwANMzffjkE"
+
+cp -rf $Path$source $Path"tmp"
+
+cd $Path"des/"
+
+cp -rf * $Path"tmp/"$name
+
+cd "$Path""tmp/""$name"
+
+zip -r "$Path""tmp/encry/""$name" *
+
+rc4tool $Path"tmp/encry/"$name $Path"apk/"$name $screte
